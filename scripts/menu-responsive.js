f5f581c05c2d6e5d77ba58d24334ccc80a2ec5d9
Felipe Leão <felipeleao.tech@gmail.com>
2024-01-22 23:01:19 -0300

    att project

--- scripts/menu-responsive.js
+++ scripts/menu-responsive.js
@@ -2,12 +2,21 @@ export default function menuResponsive() {
   const menu = document.querySelector("[data-menu]");
   const iconMenu = document.querySelectorAll(".iconMenu");
   const navbarList = document.querySelector(".header__navbar-list");
+  const navbarLinks = document.querySelectorAll(".header__navbar-list li");
+  let isActive;
 
   function transformIcon() {
     iconMenu.forEach((icon) => {
       icon.classList.toggle("change");
     });
     navbarList.classList.toggle("active");
+    isActive = true;
+    console.log(isActive);
   }
+
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", transformIcon);
+  });
+
   menu.addEventListener("click", transformIcon);
 }
